ad5c8f24037aa1beff3e640453b13df34556c4df
elizabeth johnson <elizabethjohnson@MacBook-Pro.local>
2022-08-24 12:54:48 -0400

    add mock transform

--- data_templates/mock_data.py
+++ data_templates/mock_data.py
@@ -1,19 +1,28 @@
 import torch
 from torch.utils.data import Dataset
 
+class transform_sample(self,addx=1,multy=1):
+    def __init__(self, scale_data=10, scale_label=5):
+        self.scale_data = scale_data
+        self.scale_label = scale_label
+    def __call__(self,sample):
+        data = sample[0] * scale_data
+        label = sample[1] * scale_label
+        
+
 class mock_data(Dataset):
     def __init__(self, length=100, transform=None):
         """init mock data"""
         # data 
         self.data = 100*torch.ones(length,3)
-        # target
+        # target label
         self.label = torch.ones(length,1)
         # length of data set object 
         self.length = length
         self.transform = transform
         
     def __getitem__(self,i):
-        """overwrite __getitem__ method and decide what index will return"""
+        """write __getitem__ method so will return samples by index"""
         training_sample = self.data[i],self.label[i]
         if self.transform:
             training_sample = self.transform(training_sample)
