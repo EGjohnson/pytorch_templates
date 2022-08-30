80d24bd33ebb4a857e9ce44cb77a90e39f75fc0e
elizabeth johnson <elizabethjohnson@MacBook-Pro.T-mobile.com>
2022-08-30 12:07:48 -0400

    updated mock transform main function

--- data_templates/mock_data.py
+++ data_templates/mock_data.py
@@ -1,24 +1,48 @@
 import torch
 from torch.utils.data import Dataset
 
-class TransformSample(object):
-    def __init__(self, scale_data=10, scale_label=5):
-        """ init transformation for data"""
+
+torch.manual_seed(1)
+
+# write your transforms
+class ScaleSample(object):
+    def __init__(self, scale_data=10.0, scale_label=5.0):
+        """constructor: transformation obj with attributes for your transform"""
         self.scale_data = scale_data
         self.scale_label = scale_label
     def __call__(self,sample):
+        """executer: how you want to transform your raw data"""
         data = sample[0] * self.scale_data
         label = sample[1] * self.scale_label
         return (data,label)
-        
+
+ 
+# class EncodeLabel(object):
+#     def __init__(self, high = ):
+#         """construct transformation with attributes for your transform"""
+#         self.scale_data = scale_data
+#         self.scale_label = scale_label
+#     def __call__(self,sample):
+#         """write here how you want to transform your raw data"""
+#         data = sample[0] * self.scale_data
+#         label = sample[1] * self.scale_label
+#         return (data,label)
+
+
+
+# CREATING A MOCK DATA SET OBJECT       
+# Need to subclass Dataset and customize the three below methods for dataset object:
+# 1. __init__: constructor
+# 2. __getitem__: method to get piece of data
+# 3. __len__: method to return length of data set
 
 class MockData(Dataset):
-    def __init__(self, length=100, transform=None):
-        """init mock data"""
-        # data 
-        self.data = 100*torch.ones(length,3)
-        # target label
-        self.label = torch.ones(length,1)
+    def __init__(self, length=200, transform=None):
+        """construct mock data object"""
+        # mock data 
+        self.data = 100*torch.rand(length,3) #x,y,z coordinate
+        # mock target label
+        self.label = torch.rand(length,1) # temperature
         # length of data set object 
         self.length = length
         self.transform = transform
@@ -35,9 +59,16 @@ class MockData(Dataset):
     
     
 def main():
-    transform = TransformSample()
-    mock_data = MockData(100, transform = transform)
-    print(mock_data[1]) 
+    # 1. create transform for raw data
+    scale_transform = ScaleSample(300, 0.5)
+    # 2. create mock Dataset object
+    mock_data = MockData(24, transform = scale_transform)
+    # 3. print outputs
+    print(f'the length of our mock data is {len(mock_data)}')
+    i = 12
+    print("Apply scale transform")
+    print(f'at index {i} the data  x,y,z coordinate is {mock_data[i][0]} and the temperature label is {mock_data[i][1]}')
+    print("Apply both transforms")
     
 
 if __name__ == "__main__":
